52bbe745de1ac791261ffdc1154be8f04bb6a350
Ken Lee <wewert@gmail.com>
2018-03-07 09:24:31 -0700

    config and model completed not tested

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+  selectAll: function(callback) {
+    orm.selectAll("burgers", function(res) {
+      callback(res);
+    });
+  },
+  insertOne: function(colToSearch, valOfCol, callback) {
+    orm.insertOne("burgers", colToSearch, valOfCol, function(res) {
+      callback(res);
+    });
+  },
+  updateOne: function(objColVals, condition, callback) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      callback(res);
+    });
+  }
+};
+
+module.exports = burger;
