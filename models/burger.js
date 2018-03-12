43350b0a996855960d33b6595178f63f4c568063
Ken Lee <wewert@gmail.com>
2018-03-12 14:57:19 -0600

    styling it up

--- models/burger.js
+++ models/burger.js
@@ -2,20 +2,20 @@ var orm = require("../config/orm.js");
 
 var burger = {
 
-  all: function(cb) {
-    orm.all("burgers", function(res) {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
       cb(res);
     });
   },
 
- create: function(cols, vals, cb){
-   orm.create("burgers", cols, vals, function(res){
+ insertOne: function(cols, vals, cb){
+   orm.insertOne("burgers", cols, vals, function(res){
      cb(res);
    });
  },
 
- update: function(objColVals, condition, cb) {
-   orm.update("burgers",objColVals, condition, function(res){
+ updateOne: function(objColVals, condition, cb) {
+   orm.updateOne("burgers",objColVals, condition, function(res){
      cb(res);
    });
  }
